9c7187190e7267ce2955e072680a6e1949c2a949
Rishi Srivastav <rishiaiweb@gmail.com>
2026-05-12 16:34:12 +0530

    Add files via upload

--- practice_query1.sql
+++ practice_query1.sql
@@ -37,3 +37,69 @@ DROP COLUMN ctc;
 
 SELECT * FROM  Teacher;
  
+CREATE TABLE  Student(
+  roll_no INT PRIMARY KEY,
+  name VARCHAR(50),
+  city VARCHAR(50),
+  marks INT
+);
+
+INSERT INTO Student
+(roll_no, name, city, marks)
+VALUES
+(110, "adam", "delhi", 76),
+(108,"bob","mumbai", 65),
+(124, "casey", "pune", 94),
+(112, "duke", "pune", 80);
+
+SELECT * FROM Student
+WHERE marks >75;
+
+SELECT DISTINCT city
+FROM Student;
+
+SELECT max(marks)
+FROM Student;
+
+SELECT avg(marks)
+FROM Student;
+
+ALTER TABLE Student
+ADD COLUMN grade VARCHAR(1);
+
+UPDATE Student
+SET grade = "O"
+WHERE marks >=80;
+
+UPDATE Student
+SET grade = "A"
+WHERE marks >=70 AND marks <80;
+
+UPDATE Student
+SET grade = "B"
+WHERE marks >=60 AND marks <70;
+
+
+
+SELECT * FROM Student
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
